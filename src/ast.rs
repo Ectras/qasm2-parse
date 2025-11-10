d7b9875eaa9dc93eddd38e00846ff38b7b71c371
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2025-11-10 15:29:57 +0100

    Add abstract syntax tree (AST)

---
+++ src/ast.rs
@@ -0,0 +1,467 @@
+//! Contains structs and enums that make up an Abstract Syntax Tree (AST) for QASM2.
+use std::fmt::{Debug, Display};
+
+use itertools::join;
+
+/// A unary operator type.
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum UnOp {
+    Neg,
+}
+
+impl Display for UnOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let symbol = match self {
+            Self::Neg => "-",
+        };
+        f.write_str(symbol)
+    }
+}
+
+/// A binary operator type.
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum BinOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    BitXor,
+}
+
+impl Display for BinOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let symbol = match self {
+            Self::Add => "+",
+            Self::Sub => "-",
+            Self::Mul => "*",
+            Self::Div => "/",
+            Self::BitXor => "^",
+        };
+        f.write_str(symbol)
+    }
+}
+
+impl BinOp {
+    pub const fn get_precedence(self) -> u32 {
+        match self {
+            Self::Sub => 2,
+            Self::Add => 3,
+            Self::Mul | Self::Div => 1,
+            Self::BitXor => 4,
+        }
+    }
+}
+
+/// A function type.
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum FuncType {
+    Sin,
+    Cos,
+    Tan,
+    Exp,
+    Ln,
+    Sqrt,
+}
+
+impl Display for FuncType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let symbol = match self {
+            Self::Sin => "sin",
+            Self::Cos => "cos",
+            Self::Tan => "tan",
+            Self::Exp => "exp",
+            Self::Ln => "ln",
+            Self::Sqrt => "sqrt",
+        };
+        f.write_str(symbol)
+    }
+}
+
+/// A mathematical expression.
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Int(i64),
+    Float(f64),
+    Variable(String),
+    Unary(UnOp, Box<Expr>),
+    Binary(BinOp, Box<Expr>, Box<Expr>),
+    Function(FuncType, Box<Expr>),
+}
+
+impl Display for Expr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Int(x) => write!(f, "{x}"),
+            Self::Float(x) => write!(f, "{x}"),
+            Self::Variable(x) => write!(f, "{x}"),
+            Self::Unary(op, inner) => {
+                let need_parens = matches!(**inner, Self::Binary(_, _, _));
+                if need_parens {
+                    write!(f, "{op}({inner})")
+                } else {
+                    write!(f, "{op}{inner}")
+                }
+            }
+            Self::Binary(op, lhs, rhs) => {
+                let pre = op.get_precedence();
+                let lhs_need_parens = if let Self::Binary(opl, _, _) = **lhs {
+                    opl.get_precedence() > pre
+                } else {
+                    false
+                };
+                let rhs_need_parens = if let Self::Binary(opr, _, _) = **rhs {
+                    opr.get_precedence() > pre
+                } else {
+                    false
+                };
+
+                if lhs_need_parens {
+                    write!(f, "({lhs})")?;
+                } else {
+                    write!(f, "{lhs}")?;
+                }
+                write!(f, " {op} ")?;
+                if rhs_need_parens {
+                    write!(f, "({rhs})")?;
+                } else {
+                    write!(f, "{rhs}")?;
+                }
+                Ok(())
+            }
+            Self::Function(kind, inner) => write!(f, "{kind}({inner})"),
+        }
+    }
+}
+
+impl PartialEq for Expr {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Int(l0), Self::Int(r0)) => l0 == r0,
+            (Self::Float(l0), Self::Float(r0)) => l0 == r0,
+            (Self::Variable(l0), Self::Variable(r0)) => l0 == r0,
+            (Self::Unary(l0, l1), Self::Unary(r0, r1)) => l0 == r0 && l1 == r1,
+            (Self::Binary(l0, l1, l2), Self::Binary(r0, r1, r2)) => {
+                l0 == r0 && l1 == r1 && l2 == r2
+            }
+            (Self::Function(l0, l1), Self::Function(r0, r1)) => l0 == r0 && l1 == r1,
+            _ => false,
+        }
+    }
+}
+
+/// A name with optionally a designator, e.g. `a` or `q[5]`.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Argument(pub String, pub Option<u32>);
+
+impl Display for Argument {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)?;
+        if let Some(designator) = self.1 {
+            write!(f, "[{designator}]")?;
+        }
+        Ok(())
+    }
+}
+
+/// Declaration of a gate. The body can be `None` in case of opaque gates.
+#[derive(Debug, PartialEq)]
+pub struct GateDeclaration {
+    /// The name of the gate.
+    pub name: String,
+    /// The names of the classical arguments.
+    pub params: Vec<String>,
+    /// The names of the qubit arguments.
+    pub qubits: Vec<String>,
+    /// The body or `None` for opaque gate declarations.
+    pub body: Option<Vec<Statement>>,
+}
+
+impl Display for GateDeclaration {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.body.is_some() {
+            write!(f, "gate")?;
+        } else {
+            write!(f, "opaque")?;
+        }
+        write!(f, " {}", self.name)?;
+
+        // Write params
+        if !self.params.is_empty() {
+            write!(f, "({})", join(&self.params, ", "))?;
+        }
+
+        // Write qubits
+        write!(f, " {}", join(&self.qubits, ", "))?;
+
+        // Write body
+        if let Some(body) = &self.body {
+            writeln!(f, " {{")?;
+            for statement in body {
+                writeln!(f, "    {statement}")?;
+            }
+            write!(f, "}}")
+        } else {
+            write!(f, ";")
+        }
+    }
+}
+
+/// Call of a quantum gate.
+#[derive(Debug, PartialEq)]
+pub struct GateCall {
+    /// The name of the gate to call.
+    pub name: String,
+    /// The classical arguments.
+    pub args: Vec<Expr>,
+    /// The qubits to act upon.
+    pub qargs: Vec<Argument>,
+}
+
+impl Display for GateCall {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)?;
+
+        // Write args
+        if !self.args.is_empty() {
+            write!(f, "({})", join(&self.args, ", "))?;
+        }
+
+        // Write qargs
+        write!(f, " {};", join(&self.qargs, ", "))
+    }
+}
+
+/// A QASM2 statement.
+#[derive(Debug, PartialEq)]
+pub enum Statement {
+    RegisterDeclaration {
+        is_quantum: bool,
+        name: String,
+        size: usize,
+    },
+    GateDeclaration(GateDeclaration),
+    GateCall(GateCall),
+    Measurement {
+        src: Argument,
+        dest: Argument,
+    },
+    Reset {
+        dest: Argument,
+    },
+    If {
+        variable: String,
+        condition: u32,
+        body: Box<Statement>,
+    },
+    Barrier(Vec<Argument>),
+}
+
+impl Display for Statement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::RegisterDeclaration {
+                is_quantum,
+                name,
+                size,
+            } => {
+                let kind = if *is_quantum { "qreg" } else { "creg" };
+                write!(f, "{kind} {name}[{size}];")
+            }
+            Self::GateDeclaration(data) => write!(f, "{data}"),
+            Self::GateCall(data) => write!(f, "{data}"),
+            Self::Measurement { src, dest } => write!(f, "measure {src} -> {dest};"),
+            Self::Reset { dest } => write!(f, "reset {dest};"),
+            Self::If {
+                variable,
+                condition,
+                body,
+            } => write!(f, "if {variable} = {condition} {body}"),
+            Self::Barrier(qargs) => {
+                write!(f, "barrier {};", join(qargs, ", "))
+            }
+        }
+    }
+}
+
+impl Statement {
+    /// Creates a new gate declaration statement.
+    pub fn gate_declaration<S>(
+        name: S,
+        params: Vec<String>,
+        qubits: Vec<String>,
+        body: Vec<Self>,
+    ) -> Self
+    where
+        S: Into<String>,
+    {
+        Self::GateDeclaration(GateDeclaration {
+            name: name.into(),
+            params,
+            qubits,
+            body: Some(body),
+        })
+    }
+
+    /// Creates a new gate call statement.
+    pub fn gate_call<S>(name: S, args: Vec<Expr>, qargs: Vec<Argument>) -> Self
+    where
+        S: Into<String>,
+    {
+        Self::GateCall(GateCall {
+            name: name.into(),
+            args,
+            qargs,
+        })
+    }
+}
+
+/// A QASM2 program.
+#[derive(Debug, PartialEq)]
+pub struct Program {
+    pub statements: Vec<Statement>,
+}
+
+impl Display for Program {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "OPENQASM 2.0;")?;
+        for statement in &self.statements {
+            writeln!(f, "{statement}")?;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn display_program() {
+        let program = Program {
+            statements: vec![
+                Statement::RegisterDeclaration {
+                    is_quantum: true,
+                    name: String::from("q"),
+                    size: 2,
+                },
+                Statement::RegisterDeclaration {
+                    is_quantum: false,
+                    name: String::from("c"),
+                    size: 1,
+                },
+                Statement::gate_declaration(
+                    "x",
+                    vec![String::from("a"), String::from("b")],
+                    vec![String::from("q")],
+                    vec![Statement::gate_call(
+                        "U",
+                        vec![
+                            Expr::Binary(
+                                BinOp::Add,
+                                Box::new(Expr::Variable(String::from("a"))),
+                                Box::new(Expr::Variable(String::from("b"))),
+                            ),
+                            Expr::Int(0),
+                            Expr::Variable(String::from("b")),
+                        ],
+                        vec![Argument(String::from("q"), None)],
+                    )],
+                ),
+                Statement::gate_declaration(
+                    "y",
+                    vec![String::from("a")],
+                    vec![String::from("q1"), String::from("q2")],
+                    vec![
+                        Statement::gate_call(
+                            "x",
+                            vec![Expr::Variable(String::from("a")), Expr::Int(0)],
+                            vec![Argument(String::from("q2"), None)],
+                        ),
+                        Statement::gate_call(
+                            "U",
+                            vec![Expr::Int(1), Expr::Int(2), Expr::Int(3)],
+                            vec![Argument(String::from("q1"), None)],
+                        ),
+                        Statement::gate_call(
+                            "x",
+                            vec![Expr::Int(1), Expr::Variable(String::from("a"))],
+                            vec![Argument(String::from("q1"), None)],
+                        ),
+                    ],
+                ),
+                Statement::gate_call(
+                    "y",
+                    vec![Expr::Int(2)],
+                    vec![
+                        Argument(String::from("q"), Some(0)),
+                        Argument(String::from("q"), Some(1)),
+                    ],
+                ),
+                Statement::Measurement {
+                    src: Argument(String::from("q"), Some(1)),
+                    dest: Argument(String::from("c"), Some(0)),
+                },
+            ],
+        };
+
+        let out = program.to_string();
+        assert_eq!(
+            out,
+            "\
+OPENQASM 2.0;
+qreg q[2];
+creg c[1];
+gate x(a, b) q {
+    U(a + b, 0, b) q;
+}
+gate y(a) q1, q2 {
+    x(a, 0) q2;
+    U(1, 2, 3) q1;
+    x(1, a) q1;
+}
+y(2) q[0], q[1];
+measure q[1] -> c[0];
+"
+        );
+    }
+
+    #[test]
+    fn display_long_expression() {
+        let expr = Expr::Unary(
+            UnOp::Neg,
+            Box::new(Expr::Binary(
+                BinOp::Div,
+                Box::new(Expr::Binary(
+                    BinOp::Mul,
+                    Box::new(Expr::Unary(UnOp::Neg, Box::new(Expr::Int(4)))),
+                    Box::new(Expr::Function(FuncType::Sin, Box::new(Expr::Int(2)))),
+                )),
+                Box::new(Expr::Binary(
+                    BinOp::Sub,
+                    Box::new(Expr::Int(2)),
+                    Box::new(Expr::Function(
+                        FuncType::Cos,
+                        Box::new(Expr::Unary(UnOp::Neg, Box::new(Expr::Float(1.3)))),
+                    )),
+                )),
+            )),
+        );
+        let out = expr.to_string();
+        assert_eq!(out, "-(-4 * sin(2) / (2 - cos(-1.3)))");
+    }
+
+    #[test]
+    fn display_expression_sub_add() {
+        let expr = Expr::Binary(
+            BinOp::Sub,
+            Box::new(Expr::Int(5)),
+            Box::new(Expr::Binary(
+                BinOp::Add,
+                Box::new(Expr::Int(2)),
+                Box::new(Expr::Int(3)),
+            )),
+        );
+        let out = expr.to_string();
+        assert_eq!(out, "5 - (2 + 3)");
+    }
+}

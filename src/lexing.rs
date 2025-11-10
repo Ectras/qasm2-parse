042725fa244244f36d4d67489d3f86661bebf68e
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2025-11-10 15:33:47 +0100

    Add MultiLexer for multi file lexing

--- src/lexing.rs
+++ src/lexing.rs
@@ -1,4 +1,12 @@
+use std::{
+    fs, io,
+    ops::Range,
+    path::{Path, PathBuf},
+    rc::Rc,
+};
+
 use logos::{Lexer, Logos, Skip};
+use self_cell::self_cell;
 
 /// A built-in file with standard definitions for QASM2 programs.
 static QELIB: &str = include_str!("qelib1.inc");
@@ -24,7 +32,7 @@ fn skip_to_closing_comment_token(lex: &mut Lexer<TokenKind>) -> Result<Skip, Lex
     }
 }
 
-#[derive(Logos, Debug, Clone, PartialEq)]
+#[derive(Logos, Debug, Clone, Copy, PartialEq)]
 #[logos(skip r"[ \t\r\n\f]+")]
 #[logos(skip r"//[^\n]*")]
 #[logos(subpattern int = "0|[1-9][0-9]*")]
@@ -110,4 +118,140 @@ pub enum TokenKind {
     #[token("/*", |lex| skip_to_closing_comment_token(lex))]
     BlockComment,
     Eof,
+}
+
+impl TokenKind {
+    fn is_non_trivial(&self) -> bool {
+        matches!(
+            self,
+            TokenKind::Integer | TokenKind::Float | TokenKind::String | TokenKind::Identifier
+        )
+    }
+}
+
+/// A token with location information.
+#[derive(Debug)]
+pub struct Token {
+    /// The type of this token.
+    kind: TokenKind,
+    //// The span in bytes in the source.
+    span: Range<usize>,
+    /// The text of this token if non-trivial (is `None` for e.g. SEMICOLON, DOT, ...)
+    text: Option<String>,
+    /// The file this token originates from, if any (is `None` if parsing directly from a `&str`).
+    file: Option<Rc<PathBuf>>,
+}
+
+struct TokenKindLexer<'a>(Lexer<'a, TokenKind>);
+
+self_cell!(
+    struct OwningLexer {
+        owner: String,
+        #[not_covariant]
+        dependent: TokenKindLexer,
+    }
+);
+
+impl OwningLexer {
+    /// Creates a new lexer that owns the string it lexes.
+    pub fn lexer(text: String) -> Self {
+        OwningLexer::new(text, |t| TokenKindLexer(TokenKind::lexer(t)))
+    }
+}
+
+/// A lexer for lexing multiple files.
+///
+/// If lexing should continue from a new file, call [`MultiLexer::source_file`].
+/// It will then return tokens from this file until it has been read completely and
+/// then continue with the previous file. If all files have been read, a final "End
+/// of file" token will be emitted.
+pub struct MultiLexer {
+    lexers: Vec<(Option<Rc<PathBuf>>, OwningLexer)>,
+    emitted_eof: bool,
+}
+
+impl MultiLexer {
+    fn new() -> Self {
+        Self {
+            lexers: Vec::with_capacity(2),
+            emitted_eof: false,
+        }
+    }
+
+    /// Creates a new lexer by reading from a file.
+    pub fn from_file<P>(path: P) -> io::Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref().as_os_str().to_str().unwrap();
+        let mut this = MultiLexer::new();
+        this.source_file(path)?;
+        Ok(this)
+    }
+
+    /// Creates a new lexer by reading the given text.
+    pub fn from_text(text: String) -> Self {
+        let mut this = MultiLexer::new();
+        let lexer = OwningLexer::lexer(text);
+        this.lexers.push((None, lexer));
+        this
+    }
+
+    /// Reads the contents of a new file and adds it on top of the lexer stack.
+    fn source_file(&mut self, filepath: &str) -> io::Result<()> {
+        let path = PathBuf::from(filepath);
+        let (path, source) = if filepath == "qelib1.inc" {
+            (path, QELIB.to_owned())
+        } else {
+            let path = path.canonicalize().unwrap();
+            let text = fs::read_to_string(&path)?;
+            (path, text)
+        };
+        let path = Some(Rc::new(path));
+        let lexer = OwningLexer::lexer(source);
+        self.lexers.push((path, lexer));
+        Ok(())
+    }
+}
+
+impl Iterator for MultiLexer {
+    type Item = Result<Token, LexingError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            // Get the current lexer (or return EoF / None if there are none)
+            let Some((source, lexer)) = self.lexers.last_mut() else {
+                if !self.emitted_eof {
+                    self.emitted_eof = true;
+                    return Some(Ok(Token {
+                        kind: TokenKind::Eof,
+                        span: 0..0,
+                        text: None,
+                        file: None,
+                    }));
+                } else {
+                    return None;
+                }
+            };
+
+            // Get the next token
+            let token = lexer.with_dependent_mut(|_, lexer| {
+                lexer.0.next().map(|kind_result| {
+                    kind_result.map(|kind| Token {
+                        kind,
+                        span: lexer.0.span(),
+                        text: kind.is_non_trivial().then(|| lexer.0.slice().to_owned()),
+                        file: source.to_owned(),
+                    })
+                })
+            });
+
+            // If the current lexer is exhausted, pop it and try again
+            if token.is_none() {
+                self.lexers.pop();
+            } else {
+                return token;
+            }
+        }
+    }
 }
